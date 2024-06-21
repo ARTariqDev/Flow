4a27f086bbf66b74c29ad3f0f88d504972e8602b
AhYes-a-Username <tariqmahmood.dmp@gmail.com>
2024-06-21 22:41:31 +0500

    added calendar functionality,other on the way

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Calendar from './Calendar.js';
+import Home from './Home.js';
+import { ChakraProvider, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 
 function App() {
+  const [tabIndex, setTabIndex] = useState(0); // Initialize tabIndex state with 0
+
+  // Function to handle tab change
+  const handleTabChange = (index) => {
+    setTabIndex(index); // Update tabIndex state to switch tabs
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div id="App">
+        <h1>Flow</h1>
+        <h2>The better way to manage yourself</h2>
+        <Tabs index={tabIndex} onChange={handleTabChange} variant='soft-rounded' colorScheme='blue'>
+          <TabList>
+            <Tab>Calendar</Tab>
+            <Tab>To-Do List</Tab>
+            <Tab>Timers</Tab>
+            <Tab>Alarm</Tab>
+            <Tab>Stopwatch</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Calendar />
+            </TabPanel>
+            <TabPanel>
+              <div id="container">
+                <Home />
+              </div>
+            </TabPanel>
+            {/* Add TabPanel for other tabs if needed */}
+          </TabPanels>
+        </Tabs>
+      </div>
+    </ChakraProvider>
   );
 }
 
